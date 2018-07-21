7f13b8d54a0565ed481c4e8cff171d60c2ddaf67
Maurício Szabo <mauricio.szabo@gmail.com>
2018-07-21 18:44:39 -0300

    Removed comments from Lumo

--- src/repl_tooling/repl_client/lumo.cljs
+++ src/repl_tooling/repl_client/lumo.cljs
@@ -1,58 +1,41 @@
 (ns repl-tooling.repl-client.lumo
   (:require [repl-tooling.repl-client.protocols :as repl]
             [repl-tooling.repl-client :as client]
-            [cljs.core.async :refer [chan <! >!] :refer-macros [go-loop go]]
+            [cljs.core.async :refer [<! >!] :refer-macros [go-loop go] :as async]
             [cljs.reader :as reader]
             [clojure.string :as str]))
 
-(defn- parse-output [output]
-  (let [parsed (some-> output not-empty pop)
-        out (str/join "\n" parsed)]
-    (println "PARSED!!!!")
-    (prn parsed)
-    (println "--------------")
-    (prn output)
-    (println "--------------")
-
-    (if (re-find  #"^\[\w__\d" out)
-      (let [[_ out res] (reader/read-string out)]
-        {:result res :out out})
-      {:out out})))
-
-(def ^:private buffer-txt (atom []))
 (defn- code-to-lumo [identifier code]
   (let [reader (str (gensym) "reader" (gensym))
         result (str (gensym) "result" (gensym))]
     (str "(let [" reader " (goog.string/StringBuffer.)]
             (binding [cljs.core/*print-newline* true
                       cljs.core/*print-fn* (fn [x] (.append " reader " x))]
-        (let [" result " (cljs.core/eval '" code ")]
+        (let [" result " (cljs.core/eval '" code "\n)]
           ['" identifier "
            (cljs.core/str " reader ")
-           " result "])))")))
+           (cljs.core/str " result ")])))")))
 
-(defrecord Lumo [in out socket]
+(defrecord Lumo [pending-cmds]
   repl/Repl
-  (treat-data [_ data]
-    (let [string (str data)]
-      (println "DATA" string)
-      (swap! buffer-txt #(vec (concat (some-> % not-empty pop)
-                                      (str/split (str (last %) string) #"\n"))))
-      (when (str/ends-with? string "=> ")
-        (let [output (parse-output @buffer-txt)]
-          (go (>! out output))
-          (reset! buffer-txt [])))))
+  (cmd-to-send [_ command]
+    (let [id (gensym)
+          cmd (code-to-lumo id command)]
+      (swap! pending-cmds conj (str id))
+      cmd)))
 
-  (send-command [_ command]
-    (let [sym (gensym)]
-      (println "SENDING")
-      (println (code-to-lumo sym command))
-      (.write socket (code-to-lumo sym command)))))
-                ; (let [code (
-                ;             (reader/read-string))])))
+(defn- treat-output [pending-cmds out]
+  (let [[_ match] (re-find #"^\s*\[(.+?) " out)]
+    (if (@pending-cmds match)
+      (let [[_ out result] (reader/read-string out)]
+        {:out out :result result})
+      {:out out})))
 
 (defn connect-socket! [session-name host port]
-  (let [[in out socket] (client/socket! session-name host port)
-        repl (->Lumo in out socket)]
-    (client/integrate-repl in repl socket)
-    [in out]))
+  (let [[in out] (client/socket! session-name host port)
+        pending-cmds (atom #{})
+        repl (->Lumo pending-cmds)
+        [in out] (client/integrate-repl in out repl)
+        new-out (async/map #(treat-output pending-cmds %) [out])]
+
+    [in new-out]))
