4100a0b570684423a10bdc0ab321575c20614007
Maurício Szabo <mauricio.szabo@gmail.com>
2018-10-18 15:12:24 -0300

    Fixed some CLJS issues with Lumo and exceptions

--- src/repl_tooling/repl_client/clojurescript.cljs
+++ src/repl_tooling/repl_client/clojurescript.cljs
@@ -6,9 +6,10 @@
 
 (defn evaluate-code [in pending command opts callback]
   (let [id (gensym)
-        code (str "(try (clojure.core/let [res " command
+        code (str "(pr-str (try (clojure.core/let [res " command
                   "\n] ['" id " :result (pr-str res)]) (catch :default e "
-                  "['" id " :error (pr-str e)]))\n")]
+                  "['" id " :error (pr-str {:obj e :type (.-type e) "
+                  ":message (.-message e) :trace (.-stack e)})])))\n")]
     (swap! pending assoc id callback)
     (when-let [ns-name (:namespace opts)]
       (async/put! in (str "(ns " ns-name ")")))
@@ -22,18 +23,19 @@
   (break [this id]))
 
 (defn- treat-result-of-call [out pending output-fn]
-  (let [[_ id] (re-find #"^\s*\[(.+?) " out)]
-    (if-let [callback (some->> id symbol (get @pending))]
-      (let [[_ key parsed] (reader/read-string out)]
-        (callback {key parsed})
-        (swap! pending dissoc id)
-        (output-fn {:as-text out :result parsed}))
-      (output-fn {:out out}))))
+  (if-let [callback (and (vector? out) (some->> out first (get @pending)))]
+    (let [[id key parsed] out]
+      (callback {key parsed})
+      (swap! pending dissoc id)
+      (output-fn {:as-text out :result parsed}))
+    (output-fn {:out out})))
 
 (defn- pending-evals [pending output-fn out]
-  (if (and out (re-find #"^\s*\[" out))
-    (treat-result-of-call out pending output-fn)
-    (output-fn {:out out})))
+  (try
+    (treat-result-of-call (-> out reader/read-string reader/read-string)
+                          pending output-fn)
+    (catch :defaul _
+      (output-fn {:out out}))))
 
 (defn repl [session-name host port on-output]
   (let [[in out] (client/socket! session-name host port)
