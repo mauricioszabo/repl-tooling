259a57783a4ff5a58949721515dfe7b6a46a2816
Maurício Szabo <mauricio.szabo@gmail.com>
2018-10-20 16:39:20 -0300

    Autocomplete for self-hosted CLJS

--- src/repl_tooling/repl_client/clojurescript.cljs
+++ src/repl_tooling/repl_client/clojurescript.cljs
@@ -2,7 +2,10 @@
   (:require [repl-tooling.repl-client :as client]
             [cljs.core.async :as async :refer-macros [go go-loop]]
             [cljs.reader :as reader]
-            [repl-tooling.eval :as eval]))
+            [repl-tooling.eval :as eval]
+            [repl-tooling.features.autocomplete :as f-auto]
+            [repl-tooling.repl-client.cljs.autocomplete :as cljs-auto]
+            [repl-tooling.editor-helpers :as helpers]))
 
 (defn evaluate-code [in pending command opts callback]
   (let [id (gensym)
@@ -16,11 +19,50 @@
     (async/put! in code)
     id))
 
-(defrecord Evaluator [in pending]
+(defn- generic-autocomplete [repl ns-name prefix]
+  (js/Promise. (fn [resolve]
+                 (cljs-auto/complete repl
+                                     ns-name
+                                     prefix
+                                     #(if-let [res (:result %)]
+                                        (resolve (helpers/read-result res))
+                                        (resolve []))))))
+
+(defn- lumo-autocomplete [repl ns-name prefix]
+  (js/Promise. (fn [resolve]
+                 (eval/evaluate repl
+                                `(lumo.repl/get-completions ~prefix cljs.core/js->clj)
+                                {:namespace ns-name :ignore true}
+                                #(if-let [res (:result %)]
+                                   (resolve (helpers/read-result res))
+                                   (resolve []))))))
+
+(defn- detect-autocomplete [repl ns-name text prefix row col state]
+  (let [treat (fn [{:keys [result]}]
+                (if result
+                  (swap! state assoc :autocomplete-kind :lumo)
+                  (swap! state assoc :autocomplete-kind :generic)))]
+    (.
+      (js/Promise. (fn [resolve]
+                     (eval/evaluate repl
+                                    "(require 'lumo.repl)"
+                                    {:ignore true}
+                                    treat)))
+      (then (fn []
+              (f-auto/complete repl ns-name text prefix row col))))))
+
+(defrecord Evaluator [in pending state]
   eval/Evaluator
   (evaluate [_ command opts callback]
     (evaluate-code in pending command opts callback))
-  (break [this id]))
+  (break [this id])
+
+  f-auto/AutoComplete
+  (complete [repl ns-name text prefix row col]
+    (case (:autocomplete-kind @state)
+      nil (detect-autocomplete repl ns-name text prefix row col state)
+      :lumo (lumo-autocomplete repl ns-name prefix)
+      :generic (generic-autocomplete repl ns-name prefix))))
 
 (defn- treat-result-of-call [out pending output-fn]
   (if-let [callback (and (vector? out) (some->> out first (get @pending)))]
@@ -46,4 +88,4 @@
           (pending-evals pending-cmds on-output output)
           (recur))
         (on-output nil)))
-    (->Evaluator in pending-cmds)))
+    (->Evaluator in pending-cmds (atom {}))))
