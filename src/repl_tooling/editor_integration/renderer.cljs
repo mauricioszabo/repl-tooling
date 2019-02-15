e978085a31f8156e81cf088903f9f0c53d47f5a9
Maurício Szabo <mauricio.szabo@gmail.com>
2019-02-15 00:10:17 -0200

    Will work on it

--- src/repl_tooling/editor_integration/renderer.cljs
+++ src/repl_tooling/editor_integration/renderer.cljs
@@ -4,17 +4,65 @@
 
 (defprotocol Renderable
   (tp [this])
-  (as-string [this]))
+  (ellide-fn [this ratom])
+  (children-fn [this])
+  (as-string [this])
+  (as-html [this ratom]))
 
-(defrecord Literal [obj]
-  Renderable
-  (tp [_] (cond
-            (string? obj) "string"
-            (number? obj) "number"
-            (boolean? obj) "bool"
-            (nil? obj) "nil"
-            :else "other"))
-  (as-string [_] (pr-str obj)))
+#_
+(type '(1 2))
+#_
+(type [1 2 3 4])
+#_
+(type #{})
+#_
+(type {:a 10 :b 20})
+
+; (defn- coll-object)
+
+(extend-protocol Renderable
+  cljs.core/List
+  (tp [_] "list")
+  (as-string [obj] (pr-str (concat (butlast obj) '(...))))
+  (as-html [obj ratom]
+    [:div
+     [:div {:class "list"}
+      [:span {:class "delim"} "("]
+      (doall (map (fn [e i]
+                    [:<> {:key i}
+                     (as-html e (r/cursor ratom i))
+                     [:span {:class "space"} " "]])
+                  (cond-> obj)
+                          ; (if delim?))
+                  (range)))
+      [:span {:class "delim"} ")"]]])
+
+  default
+  (tp [obj] (cond
+              (string? obj) "string"
+              (number? obj) "number"
+              (boolean? obj) "bool"
+              (nil? obj) "nil"
+              :else "other"))
+  (as-string [obj] (pr-str obj))
+  (as-html [obj _]
+    (let [tp (cond
+               (string? obj) "string"
+               (number? obj) "number"
+               (boolean? obj) "bool"
+               (nil? obj) "nil"
+               :else "other")]
+      [:span {:class tp} (pr-str obj)])))
+
+; (defrecord Literal [obj]
+;   Renderable
+;   (tp [_] (cond
+;             (string? obj) "string"
+;             (number? obj) "number"
+;             (boolean? obj) "bool"
+;             (nil? obj) "nil"
+;             :else "other"))
+;   (as-string [_] (pr-str obj)))
 
 (defn parse-result
   "Will parse a result that comes from the REPL in a r/atom so that
@@ -23,12 +71,13 @@ it'll be suitable to be rendered with `view-for-result`"
   (let [parsed (helpers/parse-result result)
         res (:result parsed)]
     (if res
-      (r/atom (->Literal res))
-      (with-meta (r/atom (->Literal (:error result))) {:error true}))))
+      (r/atom res)
+      (with-meta (r/atom (:error result)) {:error true}))))
 
 (defn view-for-result
   "Renders a view for a result. If it's an error, will return a view
 suitable for error backtraces. If it's a success, will return a success
 view. Expects a r/atom that comes from `parse-result`"
   [state]
-  [:div {:class (tp @state)} (as-string @state)])
+  (as-html @state state))
+  ; [:div {:class (tp @state)} (as-string @state)])
