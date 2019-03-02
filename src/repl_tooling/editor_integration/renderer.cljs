d2354ea0d21cf2aa18035823f371c656e08b217f
Maurício Szabo <mauricio.szabo@gmail.com>
2019-03-02 20:53:00 -0300

    Indentation

--- src/repl_tooling/editor_integration/renderer.cljs
+++ src/repl_tooling/editor_integration/renderer.cljs
@@ -93,7 +93,8 @@
     (let [more-fn (eval/get-more-fn obj)
           children (mapv #(as-renderable % repl)
                          (helpers/obj (eval/without-ellision obj)))]
-      (r/atom (->Indexed (str (helpers/tag obj) " {") (vec children) "}" "record" false more-fn repl))))
+      (r/atom (->Indexed (str (helpers/tag obj) " {") (vec children) "}" "record" 
+                         false more-fn repl))))
 
   default
   (as-renderable [obj repl]
