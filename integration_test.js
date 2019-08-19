c2484b1852a89e2d819306300161bedeac923936
Maurício Szabo <mauricio.szabo@gmail.com>
2019-08-19 19:13:12 -0300

    Fixing node finder

--- integration_test.js
+++ integration_test.js
@@ -11,18 +11,16 @@ newApp = () => {
   return app.start()
 }
 app2 = newApp()
-// app2.then(e => app3 = e)
-// app2.then(console.log)
 
 runTestAndCollectResult = async (client, idx, numTests, numFailures) => {
   const selector = `.com-rigsomelight-devcard:nth-child(${idx})`
   const testName = await client.$(selector + " a").getText()
   const failures = await client.$$(selector + ' .com-rigsomelight-devcards-fail')
 
-  console.log("\n", testName)
-  client.$(selector + ' .com-rigsomelight-devcards-fail').getText()
+  console.log(`  ${testName}`)
+  await client.$(selector + ' .com-rigsomelight-devcards-fail').getText()
     .then(console.log)
-    .catch(() => console.log("No errors on test"))
+    .catch(() => console.log("   No errors on test"))
   const totalFailures = failures.length + numFailures
   if(idx >= numTests) {
     return totalFailures
@@ -30,7 +28,7 @@ runTestAndCollectResult = async (client, idx, numTests, numFailures) => {
     return runTestAndCollectResult(client, idx + 1, numTests, totalFailures)
   }
 }
-runTestAndCollectResult(client, 1, 4, 0).then(console.log)
+// runTestAndCollectResult(client, 1, 4, 0).then(console.log)
 
 collectTest = async (client, idx, numTests, totalFailures) => {
   const selector = `.com-rigsomelight-devcards-list-group-item:nth-child(${idx})`
@@ -38,7 +36,7 @@ collectTest = async (client, idx, numTests, totalFailures) => {
   const span = `${selector} span`
   const txt = await client.$(span).getText()
   const numAsserts = parseInt(txt)
-  console.log(`Collecting ${numAsserts} tests`)
+  console.log(`\n Collecting ${numAsserts} tests`)
   await client.$(selector).click()
   const failures = await runTestAndCollectResult(client, 1, numAsserts, 0)
   const total = failures + totalFailures
@@ -50,19 +48,20 @@ collectTest = async (client, idx, numTests, totalFailures) => {
     return collectTest(client, idx + 1, numTests, total)
   }
 }
-collectTest(client, 1, 4, 0)
 
-collectTests = (client, tests) => {
-  const numTests = 1 //tests.length
-  collectTest(client, 1, 1)
-  numTests.forEach((_, i) => {
-    collectTest(
-  })
+runAllTests = async (app) => {
+  await app.client.waitForText('a.com-rigsomelight-devcards-list-group-item')
+  const tsts = await app.client.$$('a.com-rigsomelight-devcards-list-group-item')
+  console.log(`Running ${tsts.length} testcases`)
+  const failures = await collectTest(app.client, 1, tsts.length, 0)
+  console.log(`\nTOTAL Failures:`, failures)
+
+  await app.stop()
+  if(failures == 0) {
+    process.exit(0)
+  } else {
+    process.exit(1)
+  }
 }
-
-runAllTests = (app) =>
-  app.client.$$('a.com-rigsomelight-devcards-list-group-item').then(tsts =>
-    collectTests(app.client, tsts)
-  )
 
 app2.then(runAllTests)
