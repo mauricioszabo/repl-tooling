7433db42ccf04d3ec4c86aa704f3a123f798f302
Maurício Szabo <mauricio.szabo@gmail.com>
2019-09-15 16:04:59 -0300

    Fixed typo on fixture app

--- test/repl_tooling/integration/fixture_app.cljs
+++ test/repl_tooling/integration/fixture_app.cljs
@@ -4,7 +4,7 @@
 (def ^:private private-var 20)
 (def local-var 10)
 
-(defn- provate-fn [a b] (+ a b 11))
+(defn- private-fn [a b] (+ a b 11))
 (defn local-fn [a b] (+ a b 10))
 (defn main []
   (prn :CONNECTED))
