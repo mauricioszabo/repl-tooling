f2ecc9b99b706cc96b4671a6d1eb75481e4497d0
Maurício Szabo <mauricio.szabo@gmail.com>
2019-09-17 19:24:57 -0300

    Connection to Shadow-CLJS done

--- src/repl_tooling/editor_integration/embedded_clojurescript.cljs
+++ src/repl_tooling/editor_integration/embedded_clojurescript.cljs
@@ -13,53 +13,54 @@
            :message (trs error "Unknown Error")})
   nil)
 
-(defn- connect-and-update-state! [state opts upgrade-cmd]
+(defn- connect-and-update-state! [state opts target upgrade-cmd]
   (let [{:keys [notify on-result on-stdout]} opts
         {:keys [host port]} (:repl/info @state)]
     (.. (conn/connect! host port upgrade-cmd {:on-result #(and on-result (on-result %))
                                               :on-stdout #(and on-stdout (on-stdout %))})
         (then #(if-let [error (:error %)]
                  (treat-error error notify)
-                 (do (swap! state assoc :cljs/repl %) state))))))
+                 (do
+                   (swap! state assoc :cljs/repl %)
+                   (notify {:type :info
+                            :title "Connected to ClojureScript"
+                            :message (str "Connected to Shadow-CLJS target" target)})))))))
 
-(defn- choose-id! [state {:keys [prompt] :as opts} commands resolve]
+(defn- choose-id! [state {:keys [prompt] :as opts} commands]
   (.. (prompt {:title "Multiple Shadow-CLJS targets"
                :message "Choose the build target that you want to connect"
                :values (->> commands keys (map (fn [id] {:key id :value (name id)})))})
-      (then #(connect-and-update-state! state opts (->> % keyword (get commands))))
-      (then resolve)))
+      (then #(connect-and-update-state! state opts
+                                        (keyword %)
+                                        (->> % keyword (get commands))))))
 
-(defn- connect-embedded [state {:keys [get-config notify] :as opts} resolve]
+(defn- connect-embedded [state {:keys [get-config notify] :as opts}]
   (let [commands (shadow/command-for (:project-paths (get-config)))]
     (if-let [error (:error commands)]
-      (resolve (treat-error error notify))
+      (treat-error error notify)
       (case (count commands)
-        0 (resolve (treat-error :no-build-id notify))
+        0 (treat-error :no-build-id notify)
         1 (.then (connect-and-update-state! state opts
-                                            (-> commands vals first))
-                 resolve)
-        (choose-id! state opts commands resolve)))))
+                                            (-> commands keys first)
+                                            (-> commands vals first)))
+        (choose-id! state opts commands)))))
 
 (defn connect! [state {:keys [notify] :as opts}]
-  (js/Promise.
-   (fn [resolve]
-     (cond
-       (:cljs/repl @state)
-       (do
-         (resolve nil)
-         (notify {:type :warn
-                  :title "REPL already connected"
-                  :message (str "REPL is already connected.\n\n"
-                                "Please, disconnect the current REPL "
-                                "if you want to connect to another.")}))
+  (cond
+    (:cljs/repl @state)
+    (do
+      (notify {:type :warn
+               :title "REPL already connected"
+               :message (str "REPL is already connected.\n\n"
+                             "Please, disconnect the current REPL "
+                             "if you want to connect to another.")}))
 
-       (:clj/aux @state)
-       (connect-embedded state opts resolve)
+    (:clj/aux @state)
+    (connect-embedded state opts)
 
-       :else
-       (do
-         (resolve nil)
-         (notify {:type :warn
-                  :title "REPL not connected"
-                  :message (str "To connect a self-hosted REPL, "
-                                "you first need to connect a Clojure REPL")}))))))
+    :else
+    (do
+      (notify {:type :warn
+               :title "REPL not connected"
+               :message (str "To connect a self-hosted REPL, "
+                             "you first need to connect a Clojure REPL")}))))
