b8a2359ca3af71bc765ed9c89e391e8004de3b19
Maurício Szabo <mauricio.szabo@gmail.com>
2019-10-18 19:15:23 -0300

    Another way to connect to the REPL

--- src/repl_tooling/repl_client/connection.cljs
+++ src/repl_tooling/repl_client/connection.cljs
@@ -2,18 +2,27 @@
   (:require [repl-tooling.editor-helpers :as helpers]
             ["net" :as net]))
 
-; (def connection (doto (. net createConnection 4444 "localhost")
-;                       (.on "data" #(prn (str %)))
-;                       (.on "drain" #(prn :drained))
-;                       (.on "close" #(prn :DOPE))
-;                       (.write "[:repl-tooling$discover #?(:cljs :cljs :clj :clj :default :other)]\n")))
-;
-; (.end connection)
-; (.write connection
-;         "[:repl-tooling$discover #?(:cljs :cljs :clj\n   :clj\n:default :other)]\n")
-;
-; (.write connection "(prn :LOL)")
-; (.-flush connection)
-; 
-(defn connect [host port]
-  (.-createConnection net))
+(defn connect! [host port]
+  (let [buffer (atom [])
+        conn (doto (. net createConnection port host)
+                   (.on "data" #(swap! buffer conj [(-> js/Date new long) (str %)]))
+                   (.on "close" #(swap! buffer conj [(-> js/Date new long) :closed])))]
+                   ; (.write "[:repl-tooling$discover #?(:cljs :cljs :clj :clj :default :other)]\n"))]
+    {:buffer buffer
+     :conn conn}))
+
+(comment
+  (def res (connect! "localhost" 2030))
+
+  (time
+   (doseq [n (range 10)]
+     (time (.write (:conn res) "(range 100000)\n"))))
+  (time
+   (-> res :buffer deref count))
+
+  (-> res :buffer deref last)
+  (-> res :buffer deref first)
+
+  (.write (:conn res) ":cljs/quit\n")
+
+  (reset! (:buffer res) []))
