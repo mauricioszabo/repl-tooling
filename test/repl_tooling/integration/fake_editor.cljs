b8f61e1429843450fe88a43a0c55dd55d1ac11f8
Maurício Szabo <mauricio.szabo@gmail.com>
2019-11-09 23:48:25 -0300

    Moving fake editor to another NS

---
+++ test/repl_tooling/integration/fake_editor.cljs
@@ -0,0 +1,86 @@
+(ns repl-tooling.integration.fake-editor
+  (:require [clojure.string :as str]
+            [reagent.core :as r]
+            [repl-tooling.editor-integration.renderer :as render]
+            [repl-tooling.editor-integration.connection :as conn]))
+
+(defonce state (r/atom {:host "localhost"
+                        :port 2233
+                        :code "(do (defrecord Foo [a b]) (->Foo (range 20) 20))"
+                        :repls {:eval nil
+                                :aux nil}
+                        :commands {}
+                        :stdout nil
+                        :stderr nil
+                        :range [[0 0]]
+                        :eval-result (r/atom nil)}))
+
+(defn- res [{:keys [result]}]
+  (reset! (:eval-result @state) (render/parse-result result (-> @state :repls :eval)))
+  (swap! state update :stdout (fn [e] (str e "=> " (:as-text result) "\n"))))
+
+(defn evaluate []
+  (let [lines (-> @state :code str/split-lines)
+        eval-sel (-> @state :commands :evaluate-selection :command)]
+    (swap! state assoc :range [[0 0] [(-> lines count dec) (-> lines last count dec)]])
+    (eval-sel)))
+
+(defn handle-disconnect []
+  (reset! (:eval-result @state) nil)
+  (swap! state assoc
+         :repls {:eval nil :aux nil}
+         :stdout nil :stderr nil
+         :commands {}))
+
+(defn connect! []
+  (when-not (-> @state :repls :eval)
+    (.
+      (conn/connect! (:host @state) (:port @state)
+                     {:on-disconnect handle-disconnect
+                      :on-stdout #(swap! state update :stdout (fn [e] (str e %)))
+                      :on-eval res
+                      :notify identity
+                      :on-stderr #(swap! state update :stderr (fn [e] (str e %)))
+                      :editor-data #(let [code (:code @state)]
+                                      {:contents code
+                                       :filename "foo.clj"
+                                       :range (:range @state)})})
+      (then (fn [res]
+              (swap! state assoc :repls {:eval (:clj/repl @res)
+                                         :aux (:clj/aux @res)}
+                     :commands (:editor/commands @res)
+                     :stdout "" :stderr ""))))))
+
+(defn editor [state]
+  [:div
+   [:h4 "Socket REPL connections"]
+   [:p [:b "Hostname: "] [:input {:type "text" :value (:host @state)
+                                  :on-change #(->> % .-target .-value (swap! state assoc :host))}]
+       [:b " Port: "] [:input {:type "text" :value (:port @state)
+                               :on-change #(->> % .-target .-value int (swap! state assoc :port))}]]
+   [:textarea {:style {:width "100%" :height "100px"}
+               :value (:code @state)
+               :on-change #(->> % .-target .-value (swap! state assoc :code))}]
+   [:p
+    (if (-> @state :repls :eval)
+      [:span
+       [:button {:on-click evaluate}
+        "Evaluate"] " "
+       [:button {:on-click conn/disconnect!} "Disconnect!"]]
+      [:button {:on-click connect!} "Connect!"])]
+   [:p (if (-> @state :repls :eval) "Connected" "Disconnected")]
+   [:div
+    (when-let [res @(:eval-result @state)]
+      [:div
+       [:h5 "RESULT"]
+       [:pre
+        [:div {:id "result" :class "result"}
+         (render/view-for-result res)]]])]
+   (when-let [out (:stdout @state)]
+     [:div
+      [:h5 "STDOUT"]
+      [:pre out]])
+   (when-let [out (:stderr @state)]
+     [:div
+      [:h5 "STDERR"]
+      [:pre out]])])
--- test/repl_tooling/integration/ui.cljs
+++ test/repl_tooling/integration/ui.cljs
@@ -1,6 +1,7 @@
 (ns repl-tooling.integration.ui
   (:require [reagent.core :as r]
             [clojure.walk :as walk]
+            [repl-tooling.integration.fake-editor :refer [editor evaluate state connect!]]
             [repl-tooling.integration.ui-macros :as ui :include-macros true]
             [repl-tooling.editor-integration.renderer :as render]
             [clojure.test :refer [async testing is] :include-macros true]
@@ -20,90 +21,6 @@
             [repl-tooling.editor-integration.autocomplete-test]
             [repl-tooling.repl-client.connection-test]))
 
-(defonce state (r/atom {:host "localhost"
-                        :port 2233
-                        :code "(do (defrecord Foo [a b]) (->Foo (range 20) 20))"
-                        :repls {:eval nil
-                                :aux nil}
-                        :commands {}
-                        :stdout nil
-                        :stderr nil
-                        :range [[0 0]]
-                        :eval-result (r/atom nil)}))
-
-(defn disconnect! []
-  (conn/disconnect!))
-
-(defn handle-disconnect []
-  (reset! (:eval-result @state) nil)
-  (swap! state assoc
-         :repls {:eval nil :aux nil}
-         :stdout nil :stderr nil
-         :commands {}))
-
-(defn- res [{:keys [result]}]
-  (reset! (:eval-result @state) (render/parse-result result (-> @state :repls :eval)))
-  (swap! state update :stdout (fn [e] (str e "=> " (:as-text result) "\n"))))
-
-(defn connect! []
-  (when-not (-> @state :repls :eval)
-    (.
-      (conn/connect! (:host @state) (:port @state)
-                     {:on-disconnect handle-disconnect
-                      :on-stdout #(swap! state update :stdout (fn [e] (str e %)))
-                      :on-eval res
-                      :notify identity
-                      :on-stderr #(swap! state update :stderr (fn [e] (str e %)))
-                      :editor-data #(let [code (:code @state)]
-                                      {:contents code
-                                       :filename "foo.clj"
-                                       :range (:range @state)})})
-      (then (fn [res]
-              (swap! state assoc :repls {:eval (:clj/repl @res)
-                                         :aux (:clj/aux @res)}
-                     :commands (:editor/commands @res)
-                     :stdout "" :stderr ""))))))
-
-(defn- evaluate []
-  (let [lines (-> @state :code str/split-lines)
-        eval-sel (-> @state :commands :evaluate-selection :command)]
-    (swap! state assoc :range [[0 0] [(-> lines count dec) (-> lines last count dec)]])
-    (eval-sel)))
-
-(defn fake-editor [state]
-  [:div
-   [:h4 "Socket REPL connections"]
-   [:p [:b "Hostname: "] [:input {:type "text" :value (:host @state)
-                                  :on-change #(->> % .-target .-value (swap! state assoc :host))}]
-       [:b " Port: "] [:input {:type "text" :value (:port @state)
-                               :on-change #(->> % .-target .-value int (swap! state assoc :port))}]]
-   [:textarea {:style {:width "100%" :height "100px"}
-               :value (:code @state)
-               :on-change #(->> % .-target .-value (swap! state assoc :code))}]
-   [:p
-    (if (-> @state :repls :eval)
-      [:span
-       [:button {:on-click evaluate}
-        "Evaluate"] " "
-       [:button {:on-click disconnect!} "Disconnect!"]]
-      [:button {:on-click connect!} "Connect!"])]
-   [:p (if (-> @state :repls :eval) "Connected" "Disconnected")]
-   [:div
-    (when-let [res @(:eval-result @state)]
-      [:div
-       [:h5 "RESULT"]
-       [:pre
-        [:div {:id "result" :class "result"}
-         (render/view-for-result res)]]])]
-   (when-let [out (:stdout @state)]
-     [:div
-      [:h5 "STDOUT"]
-      [:pre out]])
-   (when-let [out (:stderr @state)]
-     [:div
-      [:h5 "STDERR"]
-      [:pre out]])])
-
 (cards/defcard-rg rendered-result
   (fn [result]
     [:div
@@ -117,7 +34,7 @@
   {:watch-atom true})
 
 (cards/defcard-rg fake-editor
-  fake-editor
+  editor
   state
   {:inspect-data true})
 
@@ -293,7 +210,7 @@
        (ui/assert-out #"Unable to resolve classname: SomeUnknownObject"
                       "(SomeUnknownObject.)"))
 
-     (disconnect!)
+     (conn/disconnect!)
      (done))))
 
 (defn main [])
