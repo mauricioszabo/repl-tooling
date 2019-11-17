a9e263b49f67db6f2f98adf46155c1ff043a22d3
Maurício Szabo <mauricio.szabo@gmail.com>
2019-11-17 15:53:23 -0300

    Project update

--- project.clj
+++ project.clj
@@ -1,11 +1,6 @@
 (defproject repl-tooling "0.3.0"
-  :dependencies [[org.clojure/core.async "0.4.490"]]
+  :dependencies [[org.clojure/core.async "0.4.490"]
+                 [reagent "0.8.1"]
+                 [rewrite-cljs "0.4.4"]]
 
-  :source-paths ["src"]
-
-  :plugins [[lein-cljsbuild "1.1.7"]
-            [reagent "0.8.1"]
-            [rewrite-cljs "0.4.4"]])
-
-  ; :profiles {:dev {:dependencies []
-  ;                  :source-paths ["src" "test"]}})
+  :source-paths ["src"])
