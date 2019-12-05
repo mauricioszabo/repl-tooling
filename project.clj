8f49c7866bdc09314a4edd926500849a0ad38a56
Maurício Szabo <mauricio.szabo@gmail.com>
2019-12-05 20:55:23 -0300

    Adding promesa dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject repl-tooling "0.3.0"
   :dependencies [[org.clojure/core.async "0.4.490"]
                  [reagent "0.8.1"]
+                 [funcool/promesa "4.0.2"]
                  [rewrite-cljs "0.4.4"]]
 
   :source-paths ["src"])
