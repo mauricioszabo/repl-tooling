968fa501777fb1b82f561b6b2a3b71826b32773e
Maurício Szabo <mauricio.szabo@gmail.com>
2019-12-30 20:14:02 -0300

    Adding orchestra to code

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :dependencies [[org.clojure/core.async "0.4.490"]
                  [reagent "0.8.1"]
                  [funcool/promesa "4.0.2"]
-                 [rewrite-cljs "0.4.4"]]
+                 [rewrite-cljs "0.4.4"]
+                 [orchestra "2019.02.06-1"]]
 
   :source-paths ["src"])
