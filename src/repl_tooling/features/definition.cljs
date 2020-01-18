a25c00d37e2cde96e33eaeebeb8f39dd9fc2d9ea
Maurício Szabo <mauricio.szabo@gmail.com>
2020-01-18 00:08:03 -0300

    Goto def for lots of REPL

--- src/repl_tooling/features/definition.cljs
+++ src/repl_tooling/features/definition.cljs
@@ -1,7 +1,8 @@
 (ns repl-tooling.features.definition
   (:require [repl-tooling.eval :as eval]
             [repl-tooling.editor-helpers :as editor-helpers]
-            [promesa.core :as p]))
+            [promesa.core :as p]
+            ["fs" :as fs]))
 
 (defn- cmd-for-read-jar [jar-file-name]
   `(~'clojure.core/let [[jar# path#] (~'clojure.string/split ~jar-file-name #"!/" 2)
@@ -19,6 +20,15 @@
         {:file-name file-name :line (dec line) :contents result})
       {:file-name file-name :line (dec line)})))
 
+(defn- full-file-position [meta]
+  (js/Promise.
+   (fn [resolve]
+     (fs/exists (:file meta) #(if %
+                                (resolve (assoc meta
+                                                :file-name (:file meta)
+                                                :line (-> meta :line dec)))
+                                (resolve nil))))))
+
 (defn- classpath-meta->positions [clj-repl meta]
   (p/do!
     (eval/eval clj-repl "(clojure.core/require 'clojure.java.io)")
@@ -30,12 +40,33 @@
                     "                            (.getResource (clojure.lang.RT/baseLoader))"
                     "                            .getPath)"
                     "                          (:file m))))"))))
+(defn- from-classpath [clj-aux meta]
+  (p/catch
+   (p/let [pos (classpath-meta->positions clj-aux meta)]
+     (wrap-contents clj-aux (:result pos)))
+   (fn [_] nil)))
+
+(defn- from-clr [clj-repl meta]
+  (p/let [from-repl (eval/eval clj-repl
+                               (str "(clojure.core/let [m '" meta "]"
+                                    "  (clojure.core/some->> m"
+                                    "    :file"
+                                    "    (clojure.lang.RT/FindFile)"
+                                    "    str))"))]
+    {:file-name (:result from-repl) :line (-> meta :line dec)}))
 
 (defn find-var-definition [cljs-repl clj-aux ns-name symbol-name]
-  (p/let [cmd (str "(clojure.core/meta (clojure.core/resolve `" symbol-name "))")
+  (p/let [cmd (str "(clojure.core/->> `" symbol-name " "
+                   "clojure.core/resolve "
+                   "clojure.core/meta "
+                   "clojure.core/vec "
+                   "(clojure.core/into {})"
+                   ")")
           meta (eval/eval cljs-repl cmd {:namespace ns-name :ignore true})
-          meta (select-keys (:result meta) [:name :file :status :column :line])
-          pos (classpath-meta->positions clj-aux meta)
-          with-contents (wrap-contents clj-aux (:result pos))]
-    ; (prn :POS (keys with-contents))
+          meta (select-keys (:result meta)
+                            [:name :file :status :column :line])
+          ; FIXME: use REPL detection here
+          with-contents (full-file-position meta)
+          with-contents (or with-contents (from-classpath clj-aux meta))
+          with-contents (or with-contents (from-clr clj-aux meta))]
     with-contents))
