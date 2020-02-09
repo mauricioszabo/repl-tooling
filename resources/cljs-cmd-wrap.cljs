67256ed22e0446f1b403b7e06c96fd120711733e
Maurício Szabo <mauricio.szabo@gmail.com>
2020-02-09 10:57:27 -0300

    Sending patch command to WS from CLJS side

--- resources/cljs-cmd-wrap.cljs
+++ resources/cljs-cmd-wrap.cljs
@@ -13,7 +13,11 @@
                                                            (clojure.core/str
                                                             (clojure.core/last splitted)
                                                             " (function)")]))
-                         (clojure.core/tagged-literal 'unrepl/bad-symbol [nil (pr-str js-obj)]))
+                         (if (try (cljs.reader/read-string {:default clojure.core/tagged-literal}
+                                                           (clojure.core/pr-str js-obj))
+                               (catch :default _ nil))
+                           js-obj
+                           (clojure.core/tagged-literal 'unrepl/bad-symbol [nil (clojure.core/pr-str js-obj)])))
                        {:repl-tooling/... `(quote
                                              ~(->> js-obj
                                                   js/Object.getPrototypeOf
@@ -24,7 +28,7 @@
                                                   (clojure.core/map #(clojure.core/symbol (str "." %)))))}]))
 
                    res-fn
-                   (clojure.core/fn [res change-keywords?]
+                   (clojure.core/fn res-fn [res change-keywords?]
                      (clojure.core/cond
                        (clojure.core/instance? cljs.core/ExceptionInfo res)
                        (clojure.core/tagged-literal 'error
@@ -50,12 +54,37 @@
                                                  (clojure.core/pr-str (clojure.core/name res))
                                                  "]"))
 
+                       (clojure.core/instance? js/Promise res)
+                       (clojure.core/let [id (clojure.core/gensym "patch")]
+                         (.then res
+                                (clojure.core/fn [res]
+                                  (clojure.core/let [ws (clojure.core/or
+                                                          (clojure.core/resolve 'shadow.cljs.devtools.client.browser/socket-ref)
+                                                          (clojure.core/resolve 'shadow.cljs.devtools.client.node/ws-ref)
+                                                          (clojure.core/resolve 'shadow.cljs.devtools.client.react-native/socket-ref)
+                                                          (clojure.core/resolve 'shadow.cljs.devtools.client.worker/socket-ref))]
+                                    (.send @@ws
+                                           (clojure.core/pr-str
+                                            {:type :repl/result
+                                             :id id
+                                             :patch (clojure.core/pr-str
+                                                     (clojure.core/tagged-literal
+                                                      'promise
+                                                      (if (clojure.core/record? res)
+                                                        (clojure.walk/postwalk (clojure.core/fn [a] (res-fn a false)) res)
+                                                        (if (clojure.core/coll? res)
+                                                          (clojure.walk/postwalk (clojure.core/fn [a] (res-fn a true)) res)
+                                                          (res-fn res true)))))})))))
+                         (clojure.core/tagged-literal
+                          'repl-tooling/patchable [id (clojure.core/tagged-literal 'promise '<pending>)]))
+
                        (clojure.core/keyword? res) res
                        (clojure.core/= nil res) res
                        (clojure.core/boolean? res) res
                        (clojure.core/number? res) res
                        (clojure.core/string? res) res
                        (clojure.core/regexp? res) res
+                       (clojure.core/coll? res) res
                        (clojure.core/coll? res) res
                        :else (tooling$norm$jsbeam res)))]
   (try
