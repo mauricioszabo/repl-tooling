4b5b386fa85a764c1bb3a16e456d9a0fddc1384c
Maurício Szabo <mauricio.szabo@gmail.com>
2020-04-09 00:17:49 -0300

    Removed dead code

--- src/repl_tooling/nrepl/bencode.cljs
+++ src/repl_tooling/nrepl/bencode.cljs
@@ -51,94 +51,14 @@
       :else
       [fragment acc])))
 
-(defn- change-status [state status acc-str buffer-size]
-  (swap! state #(-> %
-                    (assoc :leaf-status status)
-                    (update :acc-str str acc-str)
-                    (update :buffer subs buffer-size))))
-
-(defn- clear-status [state result]
-  (swap! state #(-> %
-                    (assoc :acc-str "" :leaf-status nil)
-                    (update :acc conj result))))
-
-(declare decode-sm)
-(defn- decode-leaf [state fragment f]
-  (let [status (:leaf-status @state)]
-    (prn :DECODING-LEAF status @state)
-    (cond
-      (and (nil? status) (re-find #"\d" f))
-      (let [[_ c term?] (re-find #"^(\d+)(:)" fragment)
-            bytes (-> c js/parseInt delay)
-            start (-> c count inc delay)]
-        (if term?
-          (do
-            (prn :WAT? @start)
-            (change-status state :s "" @start)
-            (swap! state assoc :acc-bytes 0 :bytes-expected @bytes)
-            (decode-sm state))
-          []))
-
-      (= :s status)
-      (let [{:keys [bytes-expected acc-bytes]} @state
-            cut-frag (.. Buffer (from fragment) (slice 0 (- bytes-expected acc-bytes)))
-            _ (prn :CUT cut-frag)
-            curr-bytes (+ acc-bytes (.-length cut-frag))
-            cut-str (str cut-frag)]
-        (swap! state #(-> %
-                          (assoc :acc-bytes curr-bytes)
-                          (update :acc-str str cut-str)
-                          (update :buffer subs (count cut-str))))
-        (prn :AND? curr-bytes bytes-expected)
-        (when (= curr-bytes bytes-expected)
-          (clear-status state (:acc-str @state)))
-        (decode-sm state))
-
-      (or (= status :i) (and (nil? status) (= "i" f)))
-      (let [[res value term?] (re-find #"i?(\-?\d*)(e)?" fragment)]
-        (change-status state :i value (count res))
-        (when term?
-          (clear-status state (js/parseInt (:acc-str @state))))
-        (decode-sm state))
-
-      :else
-      (throw (ex-info "Garbage on parsing bencode" {:string fragment})))))
-
-(defn- decode-sm [state]
-  (let [level (:level @state)
-        fragment (:buffer @state)
-        f (first fragment)]
-    (prn :DECODING @state)
-    (cond
-      (= "" fragment)
-      (let [acc (:acc @state)]
-        (prn :THE-END acc)
-        (swap! state assoc :acc [])
-        acc)
-
-      (:leaf-status @state)
-      (decode-leaf state fragment f)
-
-      (#{"l" "m"} f)
-      (do
-        (swap! state #(-> %
-                          (update :buffer subs 1)
-                          (update :level inc)
-                          (update :list-status conj (keyword f))
-                          (assoc :list-acc [])
-                          (update :buffer subs 1)))
-        (recur state))
-
-      (-> @state :list-status (get level) (= :l))
-      (if (= "e" f)
-        (let [result (decode-sm state)]
-          (swap! state #(-> %
-                            (update :list-acc conj result)
-                            (update :buffer subs 1)))
-          (:list-acc @state)))
-
-      :else
-      (decode-leaf state fragment f))))
+(defn old-decoder
+  []
+  (let [state (atom "")]
+    (fn [fragment]
+      (swap! state str fragment)
+      (let [[rest parsed] (decode-fragment @state [])]
+        (reset! state rest)
+        parsed))))
 
 (defn- remove-chars [state char-size]
   (swap! state update :buffer subs char-size))
@@ -263,15 +183,6 @@ Ex:
     (fn [fragment]
       (swap! state update :buffer str fragment)
       (decode-cont state []))))
-
-(defn old-decoder
-  []
-  (let [state (atom "")]
-    (fn [fragment]
-      (swap! state str fragment)
-      (let [[rest parsed] (decode-fragment @state [])]
-        (reset! state rest)
-        parsed))))
 
 ;; Performance tests
 #_
