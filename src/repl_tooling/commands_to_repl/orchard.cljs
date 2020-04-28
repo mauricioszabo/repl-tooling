8dd7631604d62bf98907c7aff0330a93dce88b62
Maurício Szabo <mauricio.szabo@gmail.com>
2020-04-27 22:01:59 -0300

    Info for CLJ working

--- src/repl_tooling/commands_to_repl/orchard.cljs
+++ src/repl_tooling/commands_to_repl/orchard.cljs
@@ -1,5 +1,7 @@
 (ns repl-tooling.commands-to-repl.orchard
+  (:require-macros [repl-tooling.repl-client.clj-helper :as h])
   (:require [repl-tooling.eval :as eval]
+            [repl-tooling.editor-helpers :as helpers]
             [promesa.core :as p]))
 
 (defn- have-ns? [repl namespace]
@@ -7,12 +9,28 @@
       (p/then (constantly true))
       (p/catch (constantly false))))
 
+(def ^:private info-msg (h/contents-for-fn "orchard-cmds.clj" "info"))
+
 (defn- info! [repl editor-state]
-  (prn :LOL!))
+  (p/let [{:keys [on-start-eval on-eval editor-data]} (:editor/callbacks @editor-state)
+          {:keys [contents range] :as ed} (editor-data)
+          eval-code (-> @editor-state :editor/features :eval-and-render)
+          start (first range)
+          id (gensym "info")
+          [_ ns-name] (helpers/ns-range-for contents start)
+          ns-name (or ns-name (p/then (eval/eval repl "(str clojure.core/*ns*)") :result))
+          [range var] (helpers/current-var contents start)
+          params {:id id :editor-data ed :range range}
+          cmd (str "(" info-msg " " (pr-str (str ns-name)) " " (pr-str var) ")")
+          [row col] start]
+    (on-start-eval params)
+    (eval/evaluate repl
+                   cmd
+                   {:ignore true :namespace ns-name :row row :col col}
+                   #(on-eval (assoc params :result %)))))
 
 (defn cmds [editor-state]
   (p/let [aux-repl (:clj/aux @editor-state)
-          _ (prn :AUX aux-repl)
           have-info? (have-ns? aux-repl "orchard.info")]
     (cond-> {}
             have-info? (assoc :info-for-var {:name "Info for var"
