733e1811ea0362516e2065f8201d9904fd2c9fc5
Maurício Szabo <mauricio.szabo@gmail.com>
2020-04-29 00:09:09 -0300

    Better "info" cmd

--- resources/orchard-cmds.clj
+++ resources/orchard-cmds.clj
@@ -1,22 +1,28 @@
-(defn info [ns-name var-name]
-  (let [s (orchard.info/info (symbol ns-name) (symbol var-name))]
+(defn info [ns-name var-name params]
+  (let [s (orchard.info/info (symbol ns-name) (symbol var-name) params)]
     (tagged-literal
      'repl-tooling/interactive
-     {:html '[:div.rows
-              [:div.title (cond->> (str (:name ?state))
-                                   (:ns ?state) (str (:ns ?state) "/"))]
-              [:div.indent (str (:arglists ?state))]
-              [:div.space]
-              [:div.pre (str (:doc ?state))]
-              [:div.space]
-              [:div.rows
-               [:div [:i "See also:"]]
-               [:<> (map (fn [e i] [:div {:key i}
-                                    [:a {:href "#"
-                                         :on-click (?info e)}
-                                     (pr-str e)]])
-                         (:see-also ?state) (range))]]]
+     {:html '(cond
+               (nil? ?state)
+               [:div.title "Nothing found for this var"]
+
+               :else
+               [:div.rows
+                [:div.title (cond->> (str (:name ?state))
+                                     (:ns ?state) (str (:ns ?state) "/"))]
+                [:div.indent (str (:arglists ?state))]
+                [:div.space]
+                [:div.pre (str (:doc ?state))]
+                [:div.space]
+                [:div.rows
+                 [:div [:i "See also:"]]
+                 [:<> (map (fn [e i] [:div {:key i}
+                                      [:a {:href "#"
+                                           :on-click (?info e)}
+                                       (pr-str e)]])
+                           (:see-also ?state) (range))]]])
       :state s
-      :fns {:info '(fn [_ s var-name]
-                     (orchard.info/info (symbol (namespace var-name))
-                                        (symbol (name var-name))))}})))
+      :fns {:info (list 'fn '[_ s var-name]
+                        (list 'orchard.info/info '(symbol (namespace var-name))
+                                                 '(symbol (name var-name))
+                              params))}})))
