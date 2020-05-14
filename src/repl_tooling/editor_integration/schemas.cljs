9cb3cf49e390b67e1d4d69b6bae129be7bb1ed64
Maurício Szabo <mauricio.szabo@gmail.com>
2020-05-14 14:00:18 -0300

    Schema fix

--- src/repl_tooling/editor_integration/schemas.cljs
+++ src/repl_tooling/editor_integration/schemas.cljs
@@ -110,8 +110,11 @@
                           (s/optional-key :clj/aux) s/Any
                           (s/optional-key :clj/repl) s/Any
                           (s/optional-key :cljs/repl) s/Any
-                          (s/optional-key :repl/info) {:host s/Str
-                                                       :port s/Int
-                                                       :kind ReplKind
-                                                       :kind-name s/Str}
+                          (s/optional-key :repl/info)
+                          {:host s/Str
+                           :port s/Int
+                           :kind ReplKind
+                           :kind-name s/Str
+                           (s/optional-key :cljs/repl-env) s/Any}
+
                           :editor/commands Commands}))
