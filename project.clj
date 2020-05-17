37b89e4f2e3a54f607d682c9b2c0cd2488fe05ec
Maurício Szabo <mauricio.szabo@gmail.com>
2020-05-17 19:46:15 -0300

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject repl-tooling "0.4.4"
+(defproject repl-tooling "0.5.0"
   :dependencies [[org.clojure/core.async "0.4.490"]
                  [reagent "0.8.1"]
                  [funcool/promesa "4.0.2"]
