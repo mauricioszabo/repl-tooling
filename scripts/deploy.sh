ff93fb19645ada1d2c49e060fbcd7f6571378cbc
Maurício Szabo <mauricio.szabo@gmail.com>
2020-06-22 21:25:06 -0300

    SUDO all the things!

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt-get install wget unzip
+sudo apt-get install wget unzip
 wget https://github.com/borkdude/babashka/releases/download/v0.1.2/babashka-0.1.2-linux-static-amd64.zip
 unzip babashka-0.1.2-linux-static-amd64.zip
-mv bb /usr/bin
+sudo mv bb /usr/bin
 ./scripts/deploy.bb
