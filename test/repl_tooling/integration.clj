96c21e90b903e253e52a91c62cda163bd2ab1d94
Maurício Szabo <mauricio.szabo@gmail.com>
2020-08-16 17:55:10 -0300

    Re-adding all tests

--- test/repl_tooling/integration.clj
+++ test/repl_tooling/integration.clj
@@ -83,9 +83,8 @@
 (defn run-tests-on-ci {:shadow/requires-server true} []
   (shadow/watch :integration)
   (shadow/watch :fixture)
-  ; (let [sh (future (shell/sh "node" "target/fixture.js"))
-  ;       failures (run-tests!)]
-  ;   (System/exit (if (zero? failures) 0 1))))
-  (System/exit 0))
+  (let [sh (future (shell/sh "node" "target/fixture.js"))
+        failures (run-tests!)]
+    (System/exit (if (zero? failures) 0 1))))
 
 #_(run-tests!)
