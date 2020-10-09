5208619007d44b2b53695fc596f4323f65f68d63
Maurício Szabo <mauricio.szabo@gmail.com>
2020-10-09 19:17:57 -0300

    Fixes on LOTs of problems with CLJs serialization

--- resources/cljs-cmd-wrap.cljs
+++ resources/cljs-cmd-wrap.cljs
@@ -4,16 +4,15 @@
                    (clojure.core/fn [js-obj]
                      (clojure.core/tagged-literal
                       'unrepl/browsable
-                      [(if (clojure.core/= js/Function (type js-obj))
-                         (let [splitted (-> js-obj .-name cljs.core/demunge
-                                            (clojure.string/split (clojure.core/re-pattern "/")))]
+                      [(if (clojure.core/= js/Function (clojure.core/type js-obj))
+                         (let [fn-name (clojure.core/-> js-obj .-name cljs.core/demunge)
+                               fn-name (if (clojure.core/empty? fn-name)
+                                         (clojure.core/pr-str js-obj)
+                                         fn-name)]
                            (clojure.core/tagged-literal 'unrepl/bad-symbol
-                                                          [(->> splitted
-                                                                butlast
-                                                                (clojure.string/join ".")
-                                                                not-empty)
+                                                          [nil
                                                            (clojure.core/str
-                                                            (clojure.core/last splitted)
+                                                            fn-name
                                                             " (function)")]))
                          (if (try (cljs.reader/read-string {:default clojure.core/tagged-literal}
                                                            (clojure.core/pr-str js-obj))
@@ -30,8 +29,17 @@
                                                   (clojure.core/map #(clojure.core/symbol (str "." %)))))}]))
 
                    res-fn
-                   (clojure.core/fn res-fn [res change-keywords?]
+                   (clojure.core/fn [res]
                      (clojure.core/cond
+                       (clojure.core/= "cljs.core/Atom" (clojure.core/pr-str (clojure.core/type res)))
+                       (clojure.core/tagged-literal 'atom
+                         (@tooling$norm$walk @res))
+
+                       ; Reagent fixes...
+                       (clojure.core/= "reagent.ratom/RAtom" (clojure.core/pr-str (clojure.core/type res)))
+                       (clojure.core/tagged-literal 'reagent.ratom/RAtom
+                         (@tooling$norm$walk @res))
+
                        (clojure.core/instance? cljs.core/ExceptionInfo res)
                        (clojure.core/tagged-literal 'error
                          {:type "cljs.core.ExceptionInfo"
@@ -46,20 +54,24 @@
                           :trace (clojure.core/->> res .-stack clojure.string/split-lines)})
 
                        (clojure.core/symbol? res)
-                       (clojure.core/symbol (clojure.core/str "#unrepl/bad-symbol [nil "
-                                                 (clojure.core/pr-str (clojure.core/str res))
-                                                 "]"))
+                       (if (clojure.core/re-find #"\s" (str res))
+                        (clojure.core/symbol (clojure.core/str "#unrepl/bad-symbol [nil "
+                                                  (clojure.core/pr-str (clojure.core/str res))
+                                                  "]"))
+                        res)
 
-                       (clojure.core/and change-keywords? (clojure.core/keyword? res))
-                       (clojure.core/symbol (clojure.core/str "#unrepl/bad-keyword ["
-                                              (clojure.core/pr-str (clojure.core/namespace res)) " "
-                                              (clojure.core/pr-str (clojure.core/name res))
-                                              "]"))
+                       (clojure.core/keyword? res)
+                       (if (clojure.core/re-find #"\s" (str res))
+                        (clojure.core/symbol (clojure.core/str "#unrepl/bad-keyword ["
+                                               (clojure.core/pr-str (clojure.core/namespace res)) " "
+                                               (clojure.core/pr-str (clojure.core/name res))
+                                               "]"))
+                        res)
 
                        (clojure.core/instance? js/Promise res)
                        (clojure.core/let [id (clojure.core/gensym "patch")]
                          (.then res
-                                (clojure.core/fn [res]
+                                (clojure.core/fn [resolved]
                                   (cljs.core/tap>
                                    (clojure.core/tagged-literal
                                     'repl-tooling/patch
@@ -67,9 +79,29 @@
                                      (clojure.core/pr-str
                                       (clojure.core/tagged-literal
                                        'promise
-                                       (@tooling$norm$walk res)))]))))
+                                       (@tooling$norm$walk resolved)))]))))
                          (clojure.core/tagged-literal
                           'repl-tooling/patchable [id (clojure.core/tagged-literal 'promise '<pending>)]))
+
+                       ;; Collections...
+                       (clojure.core/map? res)
+                       (clojure.core/let [norm (clojure.core/->> res
+                                                                 (clojure.core/map #(clojure.core/mapv @tooling$norm$walk %))
+                                                                 (clojure.core/into {}))]
+                         (if (clojure.core/record? res)
+                           (clojure.core/tagged-literal
+                            (clojure.core/symbol (clojure.core/pr-str (clojure.core/type res)))
+                            norm)
+                           norm))
+
+                       (clojure.core/vector? res)
+                       (clojure.core/mapv @tooling$norm$walk res)
+
+                       (clojure.core/set? res)
+                       (clojure.core/->> res (clojure.core/map @tooling$norm$walk) (clojure.core/into #{}))
+
+                       (clojure.core/coll? res)
+                       (clojure.core/map @tooling$norm$walk res)
 
                        (clojure.core/keyword? res) res
                        (clojure.core/= nil res) res
@@ -77,19 +109,18 @@
                        (clojure.core/number? res) res
                        (clojure.core/string? res) res
                        (clojure.core/regexp? res) res
-                       (clojure.core/coll? res) res
                        :else (tooling$norm$jsbeam res)))]
   (try
-    (clojure.core/reset! tooling$norm$walk
-                        (clojure.core/fn [res]
-                          (if (clojure.core/record? res)
-                            (clojure.walk/postwalk (clojure.core/fn [a] (res-fn a false)) res)
-                            (if (clojure.core/coll? res)
-                              (clojure.walk/postwalk (clojure.core/fn [a] (res-fn a true)) res)
-                              (res-fn res true)))))
+    (clojure.core/reset! tooling$norm$walk (clojure.core/memoize res-fn))
+                        ; (clojure.core/fn [res]
+                          ; (if (clojure.core/record? res)
+                          ;   (clojure.walk/postwalk (clojure.core/fn [a] (res-fn a false)) res)
+                            ; (if (clojure.core/coll? res)
+                            ;   (clojure.walk/postwalk (clojure.core/fn [a] (res-fn a)) res)
+                              ; (res-fn res))
 
     (clojure.core/let [res (do __COMMAND__)]
       ['tooling$eval-res '__ID__ {:result (clojure.core/pr-str (@tooling$norm$walk res))
                                   :as-text (clojure.core/pr-str (@tooling$norm$walk res))}])
-    (catch :default e ['tooling$eval-res '__ID__ {:error (clojure.core/pr-str (res-fn e true))
-                                                  :as-text (clojure.core/pr-str (res-fn e true))}])))
+    (catch :default e ['tooling$eval-res '__ID__ {:error (clojure.core/pr-str (@tooling$norm$walk e))
+                                                  :as-text (clojure.core/pr-str (@tooling$norm$walk e))}])))
