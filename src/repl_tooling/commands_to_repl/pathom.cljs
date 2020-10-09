b952b382900cbec910b91cdfc986991883416c89
Maurício Szabo <mauricio.szabo@gmail.com>
2020-10-09 17:10:11 -0300

    Starting tests to Pathom

--- src/repl_tooling/commands_to_repl/pathom.cljs
+++ src/repl_tooling/commands_to_repl/pathom.cljs
@@ -1,26 +1,34 @@
 (ns repl-tooling.commands-to-repl.pathom
   (:require [promesa.core :as p]
+            [schema.core :as s]
+            [repl-tooling.editor-integration.schemas :as schemas]
             [repl-tooling.editor-helpers :as helpers]
+            [repl-tooling.editor-integration.evaluation :as e-eval]
             [clojure.string :as str]
             [repl-tooling.editor-integration.commands :as cmds]
             [clojure.core.async :as async]
             [com.wsscode.pathom.core :as pathom]
             [com.wsscode.pathom.connect :as connect]))
 
-(connect/defresolver editor-data [_ _]
-  {::connect/output [:editor/contents :editor/filename :editor/range
-                     :editor/additional-data]}
-  (p/let [data (cmds/run-callback! (-> @chlorine.state/state :tooling-state)
-                                   :editor-data)]
-    {:editor/contents (:content data)
-     :editor/filename (:filename data)
-     :editor/range (:range data)
-     :editor/additional-data (dissoc data [:content :filename :range])}))
+(connect/defresolver editor-data [{:keys [editor-state]} _]
+  {::connect/output [:editor/data]}
+
+  (p/let [data (cmds/run-callback! editor-state :editor-data)]
+    {:editor/data data}))
+
+(connect/defresolver separate-data [_ {:editor/keys [data]}]
+  {::connect/input #{:editor/data}
+   ::connect/output [:editor/contents :editor/filename :editor/range]}
+
+  {:editor/contents (:contents data)
+   :editor/filename (:filename data)
+   :editor/range (:range data)})
 
 (connect/defresolver namespace-from-editor-data [_ {:editor/keys [contents range]}]
   {::connect/input #{:editor/contents :editor/range}
    ::connect/output [:editor/ns-range :editor/namespace]}
-  (let [[range ns] (helpers/ns-range-for contents (first range))]
+
+  (when-let [[range ns] (helpers/ns-range-for contents (first range))]
     {:editor/ns-range range
      :editor/namespace (str ns)}))
 
@@ -30,6 +38,19 @@
    ::connect/output [:repl/namespace]}
 
   {:repl/namespace (symbol namespace)})
+
+(connect/defresolver var-from-editor
+  [{:keys [editor-state]} {:editor/keys [contents range]}]
+  {::connect/input #{:editor/contents :editor/range}
+   ::connect/output [:editor/current-var :editor/current-var-range]}
+
+  (when-let [[range curr-var] (helpers/current-var contents (first range))]
+    {:editor/current-var curr-var
+     :editor/current-var-range range}))
+
+#_
+(eql (-> @chlorine.state/state :tooling-state)
+     '[:var/meta])
 
 (connect/defresolver all-namespaces
   [{:keys [editor-state ast]} _]
@@ -44,6 +65,55 @@
                                f (filter (fn [n]
                                            (-> n :repl/namespace str
                                                (str/starts-with? f)))))}))
+
+(connect/defresolver repls-from-config
+  [{:keys [editor-state ast]} {:keys [editor/config]}]
+  {::connect/input #{:editor/config}
+   ::connect/output [:repl/eval :repl/aux :repl/clj]}
+
+  (let [clj-eval (:clj/repl @editor-state)
+        clj-aux (:clj/aux @editor-state)]
+    (case (:eval-mode config)
+      :clj {:repl/eval clj-eval
+            :repl/aux clj-aux
+            :repl/clj clj-aux}
+      :cljs {:repl/eval (:cljs/repl @editor-state)
+             :repl/aux (:cljs/repl @editor-state)
+             :repl/clj clj-aux}
+      nil)))
+
+(connect/defresolver repls-from-config+editor-data
+  [{:keys [editor-state ast]} {:keys [editor/config editor/filename]}]
+  {::connect/input #{:editor/config :editor/filename}
+   ::connect/output [:repl/eval :repl/aux :repl/clj]}
+
+  (let [cljs-file? (str/ends-with? filename ".cljs")
+        cljc-file? (or (str/ends-with? filename ".cljc")
+                       (str/ends-with? filename ".cljx"))
+        clj-aux (:clj/aux @editor-state)
+        clj {:repl/eval (:clj/repl @editor-state)
+             :repl/aux clj-aux
+             :repl/clj clj-aux}
+        cljs {:repl/eval (:cljs/repl @editor-state)
+                         :repl/aux (:cljs/repl @editor-state)
+                         :repl/clj clj-aux}]
+    (cond
+      (-> config :eval-mode (= :prefer-clj))
+      (cond
+        cljc-file? clj
+        cljs-file? cljs
+        :else clj)
+
+      (-> config :eval-mode (= :prefer-cljs))
+      (if cljc-file? cljs clj))))
+    ; (case (:eval-mode config)
+    ;   :prefer-clj {:repl/eval clj-eval
+    ;                :repl/aux clj-aux
+    ;                :repl/clj clj-aux}
+    ;   :prefer-cljs {:repl/eval (:cljs/repl @editor-state)
+    ;                 :repl/aux (:cljs/repl @editor-state)
+    ;                 :repl/clj clj-aux}
+    ;   nil)))
 
 (connect/defresolver all-vars-in-ns
   [{:keys [editor-state ast]} {:keys [repl/namespace]}]
@@ -67,41 +137,92 @@
      ;    [:repl/namespace {:namespace/vars [:var/fqn (:var/meta {:keys [:file :row :line]})]}]}])
 
 (connect/defresolver fqn-var
-  [{:keys [editor-state]} {:keys [repl/namespace editor/symbol-name]}]
-  {::connect/input #{:repl/namespace :editor/symbol-name}
+  [{:keys [editor-state]} {:keys [repl/namespace editor/current-var editor/filename]}]
+  {::connect/input #{:repl/namespace :editor/current-var :editor/filename}
    ::connect/output [:var/fqn]}
 
   (p/let [{:keys [result]} (cmds/run-feature! editor-state :eval
                                               {:namespace (str namespace)
+                                               :filename filename
                                                :ignore true
-                                               :text (str "`" symbol-name)})]
+                                               :aux true
+                                               :text (str "`" current-var)})]
     {:var/fqn result}))
-
-(connect/defresolver meta-for-var
-  [{:keys [editor-state ast]} {:var/keys [fqn]}]
-  {::connect/input #{:var/fqn}
-   ::connect/output [:var/meta]}
-
-  (p/let [keys (-> ast :params :keys)
-          {:keys [result]} (cmds/run-feature! editor-state :eval
-                                              {:aux true
-                                               :ignore true
-                                               :text (str "(meta #'" fqn ")")})]
-    {:var/meta (cond-> result
-                       (coll? keys) (select-keys keys))}))
 
 #_
 (eql (-> @chlorine.state/state :tooling-state)
-     '[{([:editor/namespace "promesa.core"]
-         {:pathom/context {:editor/symbol-name "let"}})
-        [(:var/meta {:keys [:file :row]})]}])
+     '[{[:editor/current-var "fqn-var"]
+        [:var/meta]}])
 
+(connect/defresolver cljs-env [{:keys [editor-state]} _]
+  {::connect/output [:cljs/env]}
+
+  (when-let [cmd (-> @editor-state :repl/info :cljs/repl-env)]
+    (p/let [{:keys [result]} (cmds/run-feature! editor-state :eval
+                                                {:ignore true
+                                                 :aux :always
+                                                 :text (str cmd)})]
+      {:cljs/env result})))
+
+(connect/defresolver get-config [{:keys [editor-state]} _]
+  {::connect/output [:editor/config]}
+
+  (p/let [cfg (cmds/run-callback! editor-state :get-config)]
+    {:editor/config cfg}))
+
+(connect/defresolver need-cljs [{:keys [editor-state]} {:editor/keys [config filename]}]
+  {::connect/input #{:editor/config :editor/filename}
+   ::connect/output [:cljs/required?]}
+
+  {:cljs/required? (e-eval/need-cljs? config filename)})
+
+#_
+(eql (-> @chlorine.state/state :tooling-state)
+     [:cljs/required?])
+
+; (connect/defresolver meta-for-cljs-env [_ {:keys [var/meta]}]
+;   {::connect/output [:var/meta]
+;    ::connect/resolver-weights 200
+;    ::connect/sort-plan (fn [ & args] (prn :SORTING args) 200)}
+;
+;   (prn :META meta)
+;   {:var/meta {:lol "IMAMETA"}}
+;   nil)
+(connect/defresolver meta-for-var
+  [{:keys [editor-state ast]} {:keys [var/fqn editor/filename cljs/required?]}]
+  {::connect/input #{:var/fqn :editor/filename :cljs/required?}
+   ::connect/output [:var/meta]}
+
+  (p/let [keys (-> ast :params :keys)
+          res (-> (cmds/run-feature! editor-state :eval
+                                     {:aux true
+                                      :ignore true
+                                      :filename filename
+                                      :text (str "(meta #'" fqn ")")})
+                  (p/catch (constantly nil)))
+          res (if (and required? (-> res :result nil?))
+                (cmds/run-feature! editor-state :eval
+                                 {:aux :always
+                                  :ignore true
+                                  :filename filename
+                                  :text (str "(meta #'" fqn ")")})
+                res)]
+    {:var/meta (cond-> (:result res)
+                       (coll? keys) (select-keys keys))}))
 
 (def my-resolvers [;Editor resolvers
-                   editor-data namespace-from-editor-data namespace-from-editor
+                   editor-data separate-data
+                   namespace-from-editor-data namespace-from-editor var-from-editor
+                   need-cljs get-config
+
                    ; Namespaces resolvers
                    all-namespaces all-vars-in-ns
+
+                   ; REPLs resolvers
+                   repls-from-config repls-from-config+editor-data
+
                    ; Vars resolvers
+                   cljs-env
                    fqn-var meta-for-var])
 
 (def parser
@@ -116,7 +237,7 @@
                        pathom/error-handler-plugin
                        pathom/trace-plugin]}))
 
-(defn eql [editor-state query]
+(s/defn eql :- js/Promise [editor-state :- schemas/EditorState, query]
   (let [p (p/deferred)]
     (async/go
       (try
---
+++ test/repl_tooling/editor_integration/pathom_test.cljs
@@ -0,0 +1,83 @@
+(ns repl-tooling.editor-integration.pathom-test
+  (:require [devcards.core :as cards]
+            [repl-tooling.commands-to-repl.pathom :as pathom]
+            [clojure.test]
+            [promesa.core :as p]
+            [repl-tooling.integration.fake-editor :as fake]
+            [check.async :refer [async-test check testing]]
+            [repl-tooling.repl-client.clojure :as clj]
+            [repl-tooling.repl-client.shadow-ws :as shadow-ws]))
+
+(def config (atom {:eval-mode :prefer-clj
+                   :project-paths [(. js/process cwd)]}))
+
+(def clj-repls {:repl/eval #(instance? clj/Evaluator %)
+                :repl/aux #(instance? clj/Evaluator %)
+                :repl/clj #(instance? clj/Evaluator %)})
+(def cljs-repls {:repl/eval #(instance? shadow-ws/ShadowCLJS %)
+                 :repl/aux #(instance? shadow-ws/ShadowCLJS %)
+                 :repl/clj #(instance? clj/Evaluator %)})
+
+(cards/deftest pathom-resolver
+  (async-test "pathom resolvers" {:teardown (fake/disconnect!)}
+    (fake/connect! {:get-config #(deref config)})
+
+    (testing "resolves editor data"
+      (swap! fake/state assoc :filename "somefile.cljc" :range [[0 0] [0 0]])
+      (fake/type "(ns foo)\n\n(str 1 2)")
+      (check (pathom/eql (-> @fake/state :editor-state)
+                         [:editor/contents :editor/filename :editor/range])
+             => {:editor/contents "(ns foo)\n\n(str 1 2)"
+                 :editor/filename "somefile.cljc"
+                 :editor/range [[0 0] [0 0]]}))
+
+    (testing "derives information from the editor data"
+      (swap! fake/state assoc :range [[2 1] [2 1]])
+      (check (pathom/eql (-> @fake/state :editor-state)
+                         [:editor/namespace :editor/ns-range
+                          :editor/current-var :editor/current-var-range])
+             => {:editor/namespace "foo"
+                 :editor/ns-range [[0 0] [0 7]]
+                 :editor/current-var "str"
+                 :editor/current-var-range [[2 1] [2 3]]}))
+
+    (testing "will get Clojure REPLs info from config only"
+      (swap! config assoc :eval-mode :clj)
+      (check (pathom/eql (:editor-state @fake/state) [:repl/eval :repl/aux :repl/clj])
+             => clj-repls))
+
+    (testing "will get ClojureScript REPLs info from config only"
+      (fake/run-command! :connect-embedded)
+      (swap! config assoc :eval-mode :cljs)
+      (check (pathom/eql (:editor-state @fake/state) [:repl/eval :repl/aux :repl/clj])
+             => cljs-repls))
+
+    (testing "will get Clojure REPL info from config + editor data"
+      (swap! fake/state assoc :filename "somefile.cljc")
+      (swap! config assoc :eval-mode :prefer-clj)
+      (check (pathom/eql (:editor-state @fake/state) [:repl/eval :repl/aux :repl/clj])
+             => clj-repls)
+
+      (swap! config assoc :eval-mode :prefer-cljs)
+      (swap! fake/state assoc :filename "somefile.clj")
+      (check (pathom/eql (:editor-state @fake/state) [:repl/eval :repl/aux :repl/clj])
+             => clj-repls))
+
+    (testing "will get ClojureScript REPL info from config + editor data"
+      (swap! fake/state assoc :filename "somefile.cljc")
+      (swap! config assoc :eval-mode :prefer-cljs)
+      (check (pathom/eql (:editor-state @fake/state) [:repl/eval :repl/aux :repl/clj])
+             => cljs-repls)
+
+      (swap! config assoc :eval-mode :prefer-clj)
+      (swap! fake/state assoc :filename "somefile.cljs")
+      (check (pathom/eql (:editor-state @fake/state) [:repl/eval :repl/aux :repl/clj])
+             => cljs-repls))))
+    ; TODO: Test all namespaces
+    ; TODO: Test all namespaces in CLJS
+    ; TODO: Test all vars in NS
+    ; TODO: Test all vars in NS in CLJS
+
+(cards/defcard-rg fake-editor
+  fake/editor
+  fake/state)
