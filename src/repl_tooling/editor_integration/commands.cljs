447095ea3c9f6dcec791ca0c2f094af74daf23f1
Maurício Szabo <mauricio.szabo@gmail.com>
2020-10-16 19:30:39 -0300

    Allowing running cmds from quasi-state

--- src/repl_tooling/editor_integration/commands.cljs
+++ src/repl_tooling/editor_integration/commands.cljs
@@ -1,6 +1,7 @@
 (ns repl-tooling.editor-integration.commands
   (:require [repl-tooling.editor-integration.schemas :as schemas]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [reagent.ratom :as ratom]))
 
 (defn- run-cmd [cmd state key args]
   (let [callback (get-in @state [key cmd])]
@@ -17,15 +18,34 @@
       res)))
 
 (def CallbackCmd (apply s/enum (keys schemas/Callbacks)))
-(s/defn run-callback!
+(s/defn state-run-callback!
   "Calls a function registered when the REPL was connected"
-  [state :- schemas/EditorState, cmd :- CallbackCmd, & args]
-  ; (prn :running-callback cmd)
+  [state :- schemas/EditorState, cmd :- CallbackCmd, args]
   (run-cmd cmd state :editor/callbacks args))
 
 (def FeatureCmd (apply s/enum (keys schemas/EditorFeatures)))
-(s/defn run-feature!
+(s/defn state-run-feature!
   "Runs an editor command"
-  [state :- schemas/EditorState, cmd :- FeatureCmd, & args]
-  ; (prn :running-feature cmd)
+  [state :- schemas/EditorState, cmd :- FeatureCmd, args]
   (run-cmd cmd state :editor/features args))
+
+(defprotocol CommandRunner
+  (-run-callback! [this callback args])
+  (-run-feature! [this feature args]))
+
+(extend-protocol CommandRunner
+  ratom/RAtom
+  (-run-callback! [this callback args]
+    (state-run-callback! this callback args))
+  (-run-feature! [this feature args]
+    (state-run-feature! this feature args)))
+
+(defrecord Callbacks [quasi-state]
+  CommandRunner
+  (-run-callback! [this callback args]
+    (run-cmd callback quasi-state :editor/callbacks args))
+  (-run-feature! [this feature args]
+    (run-cmd feature quasi-state :editor/features args)))
+
+(defn run-callback! [this callback & args] (-run-callback! this callback args))
+(defn run-feature! [this feature & args] (-run-feature! this feature args))
