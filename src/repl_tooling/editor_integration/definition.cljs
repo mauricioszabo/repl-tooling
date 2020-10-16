c25313b5338ae0f6a54976879581bd5c9b9bd34e
Maurício Szabo <mauricio.szabo@gmail.com>
2020-10-16 19:29:36 -0300

    goto-var working with Kondo

--- src/repl_tooling/editor_integration/definition.cljs
+++ src/repl_tooling/editor_integration/definition.cljs
@@ -2,7 +2,10 @@
   (:require [repl-tooling.editor-integration.evaluation :as e-eval]
             [repl-tooling.features.definition :as def]
             [repl-tooling.editor-helpers :as helpers]
-            [repl-tooling.editor-integration.commands :as cmds]))
+            [repl-tooling.editor-integration.commands :as cmds]
+            [promesa.core :as p]
+            [clojure.string :as str]
+            ["os" :refer [platform]]))
 
 (defn goto-var [{:keys [var-name namespace repl state]}]
   (when-not
@@ -18,3 +21,27 @@
         [_ namespace] (helpers/ns-range-for contents (first range))
         repl (e-eval/repl-for state filename true)]
     (goto-var {:var-name var :namespace namespace :repl repl :state state})))
+
+(defn- norm-result [file-name]
+  (cond-> file-name
+          (and (re-find #"^win\d+" (platform)))
+          (str/replace-first #"^/" "")))
+
+(defn- resolve-var-path [state]
+  (p/let [{:keys [run-feature run-callback]} @state
+          {:keys [var/meta editor/data]} (run-feature :eql [:editor/data :var/meta])
+          meta (select-keys meta [:file :line :column])
+          k [:editor/data data]
+          repl-res (run-feature :eql [{k [:repl/clj]}])
+          repl (get-in repl-res [k :repl/clj])
+          result (def/resolve-possible-path repl meta)]
+    (cond-> (dissoc result :file)
+            (:file-name result) (update :file-name norm-result)
+            (:column result) (update :column dec))))
+
+(defn goto-current-var' [state]
+  (let [{:keys [run-callback]} @state]
+    (-> (resolve-var-path state)
+        (p/then #(run-callback :open-editor %))
+        (p/catch #(run-callback :notify
+                                {:type :error :title "Could not find definition for var"})))))
