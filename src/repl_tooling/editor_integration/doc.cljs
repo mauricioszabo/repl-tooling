72bfb38c118491992d73ab8ec5e1edbe311a555a
Maurício Szabo <mauricio.szabo@gmail.com>
2020-10-26 16:37:39 -0300

    Re-doing doc-for-var based on Pathom

--- src/repl_tooling/editor_integration/doc.cljs
+++ src/repl_tooling/editor_integration/doc.cljs
@@ -7,72 +7,16 @@
             [repl-tooling.editor-integration.evaluation :as e-eval]
             [repl-tooling.commands-to-repl.pathom :as pathom]))
 
-(defn- doc-cmd [var filename]
-  `(~'clojure.core/let
-     [v# (~'clojure.core/or (~'clojure.core/resolve '~var)
-                            (throw
-                              (~'clojure.core/ex-info
-                                (~'clojure.core/str
-                                  "Unable to resolve var: '" '~var "' in this context in file ")
-                               {:var '~var :filename ~filename})))
-      m# (~'clojure.core/meta v#)]
-     (~'clojure.core/str "-------------------------\n"
-                         (:ns m#) "/" (:name m#) "\n"
-                         (:arglists m#) "\n  "
-                         (:doc m#))))
-
-(defn- spec-cmd [var]
-  (str "
-    (clojure.core/when-let [s (clojure.spec.alpha/get-spec '" var ")]
-      (clojure.core/some->> [:args :ret :fn]
-        (clojure.core/map #(clojure.core/some->> (% s)
-                              clojure.spec.alpha/describe
-                              clojure.core/pr-str
-                              (clojure.core/str (clojure.core/name %)\": \")))
-        (clojure.core/remove clojure.core/nil?)
-        clojure.core/not-empty
-        (clojure.core/interpose \"\\n\")
-        (clojure.core/apply str)))"))
-
-(defn- emit-result [document-part spec-part {:keys [opts eval-data repl]}]
-  (let [docs (cond-> document-part spec-part (str "\n\nSpec:\n" spec-part))
-        {:keys [on-eval]} opts
-        res {:result (pr-str docs) :literal true :as-text (pr-str docs)}]
-    (on-eval (assoc eval-data :result (helpers/parse-result res) :repl repl))))
-
-(defn- try-spec [document-part options]
-  (let [spec-ed (p/let [_ (eval/eval (:repl options)
-                                     "(clojure.core/require '[clojure.spec.alpha])")
-                        cmd (spec-cmd (:var options))]
-                  (eval/eval (:repl options) cmd))]
-    (.. spec-ed
-        (then #(emit-result document-part (:result %) options))
-        (catch #(emit-result document-part nil options)))))
-
-(defn- treat-error [error {:keys [opts eval-data repl]}]
-  (let [{:keys [on-eval]} opts]
-    (on-eval (assoc eval-data
-                    :result {:error error
-                             :parsed? true
-                             :as-text (pr-str error)}
-                    :repl repl))))
-
-(defn- run-documentation-code [{:keys [var editor-data repl] :as options}]
-  (let [on-start (-> options :opts :on-start-eval)]
-    (on-start (:eval-data options)))
-  (p/catch (p/let [var (eval/eval repl (str "`" var) {:namespace (:ns options) :ignore true})
-                   document-part (eval/eval repl (doc-cmd (:result var)
-                                                          (:filename editor-data)))]
-              (if document-part
-                (try-spec (:result document-part) (assoc options :var (:result var)))
-                (treat-error "\"Unknown error\"" options)))
-           #(treat-error (:error %) options)))
-
-(defn- translate-to-doc [meta]
+(defn- translate-to-doc [meta spec?]
   (str "-------------------------\n"
        (:ns meta) "/" (:name meta) "\n"
        (:arglists meta) "\n  "
-       (:doc meta)))
+       (:doc meta)
+       (when spec?
+         (cond-> "\nSpec\n"
+                 (:args spec?) (str "  args: " (pr-str (:args spec?)) "\n")
+                 (:ret spec?) (str "  ret: " (pr-str (:ret spec?)) "\n")
+                 (:fn spec?) (str "  fn: " (pr-str (:fn spec?)))))))
 
 (defn doc-for-var [state]
   (p/let [id (gensym "doc-for-var")
@@ -85,10 +29,10 @@
                                           :editor-data data
                                           :range current-var-range})
           k [:editor/data data]
-          res (run-feature :eql [{k [:var/meta]}])
-          {:keys [var/meta]} (get res k)
+          res (run-feature :eql [{k [:var/meta :var/spec]}])
+          {:keys [var/meta var/spec]} (get res k)
           doc (if (map? meta)
-                {:result (helpers/LiteralRender. (translate-to-doc meta))}
+                {:result (helpers/LiteralRender. (translate-to-doc meta spec))}
                 {:error "Can't find doc for this variable"})]
     (run-callback :on-eval {:id id
                             :repl nil
