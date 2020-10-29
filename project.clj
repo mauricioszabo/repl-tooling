3abd320cc00f576222d686d4aaa28fe1a7db536f
Maurício Szabo <mauricio.szabo@gmail.com>
2020-10-29 15:10:06 -0300

    Pathom version bump

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [rewrite-cljs "0.4.4"]
                  [paprika "0.1.3"]
                  [borkdude/sci "0.1.1-alpha.7"]
-                 [com.wsscode/pathom "2.3.0-alpha10"]
+                 [com.wsscode/pathom "2.3.0-alpha17"]
                  [org.pinkgorilla/gorilla-renderable-ui "0.1.33"]]
 
   :source-paths ["src"])
