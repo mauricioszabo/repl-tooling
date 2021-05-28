d8f93d4ab989c5659324f6e8ed785339ee1431e8
Maurício Szabo <mauricio.szabo@gmail.com>
2021-05-28 19:54:15 -0300

    Fixed tests for definition

--- test/repl_tooling/features/definition_test.cljs
+++ test/repl_tooling/features/definition_test.cljs
@@ -27,52 +27,51 @@
     (testing "getting var definition from core locations"
       (swap! fake/state assoc :range [[2 1] [2 1]] :code "(ns user)\n\n(prn 1 2)")
       (check (fake/run-feature! :eql
-                                {:editor-state (:editor-state @fake/state)}
-                                [:definition/file-name :definition/row :definition/file-contents])
-             => {:definition/file-name #"clojure.*jar!/clojure/core.clj"
+                                [:definition/filename :definition/row :definition/file-contents])
+             => {:definition/filename #"clojure.*jar!/clojure/core.clj"
                  :definition/row number?
                  :definition/file-contents string?}))
 
     (testing "finds definition of namespace"
       (swap! fake/state assoc :range [[0 4] [0 4]]
              :code "(ns repl-tooling.features.definition-helper)")
-      (check (fake/run-feature! :eql [:definition/file-name :definition/row])
-             => {:definition/row 0
-                 :definition/file-name #"repl_tooling/features/definition_helper\.clj"}))
+      (check (fake/run-feature! :eql [:definition/filename :definition/row])
+             => {:definition/row number?
+                 :definition/filename #"repl_tooling/features/definition_helper\.clj"}))
 
     (testing "finds symbols inside jars, and get file's contents"
       (swap! fake/state assoc :range [[2 1] [2 1]] :code "(ns user)\n\n(prn 1 2)")
       (check (fake/run-feature! :eql [:definition/file-contents
-                                      :definition/file-name
+                                      :definition/filename
                                       :definition/row])
              => {:definition/row number?
-                 :definition/file-name string?
+                 :definition/filename string?
                  :definition/file-contents string?}))
 
     (testing "finds symbols inside other namespaces, and gets file"
       (swap! fake/state assoc :range [[2 1] [2 1]]
              :code "(ns repl-tooling.features.definition-helper)\n\nc/some-function")
-      (check (fake/run-feature! :eql [:definition/file-name :definition/row])
-             => {:definition/row number?
-                 :definition/file-name #"repl_tooling/features/definition_child\.clj"})
+      (check (fake/run-feature! :eql [:definition/filename :definition/row])
+             => {:definition/row 0
+                 :definition/filename #"repl_tooling/features/definition_child\.clj"})
 
       (fake/type "(ns repl-tooling.features.definition-helper)\n\nother-var")
-      (check (fake/run-feature! :eql [:definition/file-name :definition/row])
+      (check (fake/run-feature! :eql [:definition/filename :definition/row])
              => {:definition/row 7
-                 :definition/file-name #"repl_tooling/features/definition_child\.clj"}))
+                 :definition/filename #"repl_tooling/features/definition_child\.clj"}))
 
     (testing "finds symbols inside same namespace, and gets file"
       (fake/type "(ns repl-tooling.features.definition-helper)\n\nsome-function")
-      (check (fake/run-feature! :eql [:definition/file-name :definition/row])
+      (check (fake/run-feature! :eql [:definition/filename :definition/row])
              => {:definition/row 3
-                 :definition/file-name #"repl_tooling/features/definition_helper\.clj"}))
+                 :definition/filename #"repl_tooling/features/definition_helper\.clj"}))
 
     (swap! config assoc :eval-mode :cljs)
     (testing "getting definition on current NS for ClojureScript"
       (fake/type "(ns repl-tooling.integration.fixture-app)\n\nlocal-fn")
-      (check (fake/run-feature! :eql [:definition/file-name :definition/row])
+      (check (fake/run-feature! :eql [:definition/filename :definition/row])
              => {:definition/row 9
-                 :definition/file-name #"test/repl_tooling/integration/fixture_app\.cljs"}))
+                 :definition/filename #"test/repl_tooling/integration/fixture_app\.cljs"}))
 
     (testing "getting path of stacktrace"
       (fake/type "(ns repl-tooling.integration.fixture-app)\n\nlocal-fn")
@@ -80,6 +79,6 @@
                                 {:ex/function-name "clojure.core/fn/eval1234"
                                  :ex/filename "core.clj"
                                  :ex/row 9}
-                                [:definition/file-name :definition/row])
-             => {:definition/row 8
-                 :definition/file-name #"clojure.*jar!/clojure/core.clj"}))))
+                                [:definition/filename :definition/row])
+             => {:definition/row 9
+                 :definition/filename #"clojure.*jar!/clojure/core.clj"}))))
