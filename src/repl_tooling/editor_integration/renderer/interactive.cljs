398cd49629fc9495759c6190125f76003c00e7b1
Maurício Szabo <mauricio.szabo@gmail.com>
2021-06-02 17:37:05 -0300

    Caching EQL commands and eval-ing with EQL

--- src/repl_tooling/editor_integration/renderer/interactive.cljs
+++ src/repl_tooling/editor_integration/renderer/interactive.cljs
@@ -17,7 +17,8 @@
       (nil? obj)
       (regexp? obj)
       (symbol? obj)
-      (keyword? obj)))
+      (keyword? obj)
+      (tagged-literal? obj)))
 
 (defn- norm-evt [obj]
   (->> obj
@@ -30,33 +31,52 @@
        (filter (comp edn? second))
        (into {})))
 
-(defn- run-evt-fun! [e fun state repl additional-args]
+(defn- run-evt-fun! [eql e fun state repl additional-args]
   (.preventDefault e)
   (.stopPropagation e)
-  (.. (eval/eval repl
-                 (str "(" fun " '"
-                      (pr-str (norm-evt (.-target e)))
-                      " '" (pr-str @state)
-                      " " (->> additional-args (map #(str "'"(pr-str %))) (str/join " "))
-                      ")")
-                 {:ignore true})
-      (then #(reset! state (:result %)))))
+  (let [norm #(if (and (tagged-literal? %) (-> % .-tag (= 'tooling/eval)))
+                (pr-str (.-form %))
+                (str "'" (pr-str %)))
+        code (str "(" fun " '"
+                  (pr-str (norm-evt (.-target e)))
+                  " '" (pr-str @state)
+                  " " (->> additional-args (map norm) (str/join " "))
+                  ")")
+        res (eql {:text/contents code} [:repl/result])]
+    (p/then res (fn [response]
+                  (reset! state (-> response :repl/result :result))))))
 
-(defn- prepare-fn [fun state repl]
+;; TODO: Migrate this to a better place!
+(defn- prepare-new-eql [editor-state]
+  (let [eql (-> @editor-state :editor/features :eql)
+        cached-result (eql [:editor/data :config/repl-kind
+                            :config/eval-as :config/project-paths
+                            :repl/evaluators])]
+    (fn q
+      ([query] (q {} query))
+      ([seed query]
+       (p/let [original-seed cached-result]
+         (eql (merge original-seed seed) query))))))
+
+(defn- prepare-fn [eql fun state repl]
   (fn [ & args]
     (if (-> args first edn?)
-      (fn [e] (run-evt-fun! e fun state repl args))
-      (run-evt-fun! (first args) fun state repl []))))
+      (fn [e] (run-evt-fun! eql e fun state repl args))
+      (run-evt-fun! eql (first args) fun state repl []))))
 
 (defn- bindings-for [editor-state state fns repl]
-  (let [binds (coll/map-values (fn [v] (fn [ & args]
+  (let [eql (prepare-new-eql editor-state)
+        binds (coll/map-values (fn [v] (fn [ & args]
                                          (p/do! (apply v args))
                                          nil))
                                (int/debug-bindings editor-state))
-        binds (assoc binds '?state @state '?state-atom state)]
+        binds (assoc binds
+                     '?state @state
+                     '?state-atom state
+                     'eval #(tagged-literal 'tooling/eval %))]
     (->> fns
          (map (fn [[f-name f-body]] [(->> f-name name (str "?") symbol)
-                                     (prepare-fn f-body state repl)]))
+                                     (prepare-fn eql f-body state repl)]))
          (into binds))))
 
 (defn- treat-error [hiccup]
