cc264f9a6b04bf25cb0aa2ab93efc3ea38586235
Maurício Szabo <mauricio.szabo@gmail.com>
2021-06-12 00:48:39 -0300

    Fixed Clojerl stacktraces

--- resources/generic_blob.clj
+++ resources/generic_blob.clj
@@ -3,10 +3,10 @@
                      res (___repl-tooling.__generic_printer_blob/serialize res)]
     ['tooling$eval-res '__ID__ {:result (clojure.core/pr-str res)
                                 :as-text (clojure.core/pr-str res)}])
-  (catch __EX_TYPE__ e
+  (catch __EX_TYPE__ e #?(:clje :stack) #?(:clje st)
     (clojure.core/let [ex #?(:clje (clojure.core/tagged-literal
                                     'error
-                                    (___repl-tooling.__generic_printer_blob/normalize-error e))
+                                    (___repl-tooling.__generic_printer_blob/normalize-error e st))
                              :default (___repl-tooling.__generic_printer_blob/serialize e))]
       ['tooling$eval-res '__ID__ {:error (clojure.core/pr-str ex)
                                   :as-text (clojure.core/pr-str ex)}])))
